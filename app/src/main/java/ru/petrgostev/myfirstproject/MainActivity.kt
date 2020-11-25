b2721b6c53d5ff289876db46977d7d641bbbf68a
PetrGostev <barsik@gmail.competrgostev@yandex.ru>
2020-11-25 17:18:56 +0300

    commit_1

--- app/src/main/java/ru/petrgostev/myfirstproject/MainActivity.kt
+++ app/src/main/java/ru/petrgostev/myfirstproject/MainActivity.kt
@@ -4,8 +4,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            addFragments()
+        }
+    }
+
+    private fun addFragments() {
+        val fragmentManager = supportFragmentManager
+
+        fragmentManager.beginTransaction().apply {
+            add(R.id.fame, MoviesListFragment())
+            commit()
+        }
+
+        fragmentManager.beginTransaction().apply {
+            replace(R.id.fame, MoviesDetailsFragment())
+            addToBackStack("MoviesDetailsFragment")
+            commit()
+        }
     }
 }
---
+++ app/src/main/java/ru/petrgostev/myfirstproject/MoviesListFragment.kt
@@ -0,0 +1,5 @@
+package ru.petrgostev.myfirstproject
+
+import androidx.fragment.app.Fragment
+
+class MoviesListFragment : Fragment(R.layout.fragment_movies_list) {}
