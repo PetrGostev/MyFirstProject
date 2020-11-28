a6c5f670da9e79b5ac6cd2d5b95fd6984a9d9669
PetrGostev <barsik@gmail.competrgostev@yandex.ru>
2020-11-28 11:19:19 +0300

    remix

--- app/src/main/java/ru/petrgostev/myfirstproject/MainActivity.kt
+++ app/src/main/java/ru/petrgostev/myfirstproject/MainActivity.kt
@@ -10,22 +10,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         if (savedInstanceState == null) {
-            addFragments()
-        }
-    }
-
-    private fun addFragments() {
-        val fragmentManager = supportFragmentManager
-
-        fragmentManager.beginTransaction().apply {
-            add(R.id.fame, MoviesListFragment())
-            commit()
-        }
-
-        fragmentManager.beginTransaction().apply {
-            replace(R.id.fame, MoviesDetailsFragment())
-            addToBackStack("MoviesDetailsFragment")
-            commit()
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fame, MoviesListFragment())
+                .commit()
         }
     }
 }
