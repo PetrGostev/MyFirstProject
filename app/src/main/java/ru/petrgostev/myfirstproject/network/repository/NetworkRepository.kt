d1cb3288c2fe4207e031fd40ff4951c7e008d258
PetrGostev <barsik@gmail.competrgostev@yandex.ru>
2021-01-17 12:59:40 +0300

    Revert "to LifeData"
    This reverts commit f33d92b93646cba20eafc1106243988388813e41.

--- app/src/main/java/ru/petrgostev/myfirstproject/network/repository/NetworkRepository.kt
+++ app/src/main/java/ru/petrgostev/myfirstproject/network/repository/NetworkRepository.kt
@@ -1,10 +1,8 @@
 package ru.petrgostev.myfirstproject.network.repository
 
-import androidx.lifecycle.LiveData
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
-import androidx.paging.liveData
 import kotlinx.coroutines.flow.Flow
 import ru.petrgostev.myfirstproject.moviesList.padding.MoviesPagingSource
 import ru.petrgostev.myfirstproject.network.api.NetworkModule
@@ -18,11 +16,11 @@ class NetworkRepository(private val networkModule: NetworkModule) : NetworkRepos
 
     override suspend fun getGenres(): List<GenresItem> = networkModule.genreApi.getAll().genres
 
-    override  fun getMovies(sort: Category): LiveData<PagingData<MoviesItem>> {
+    override fun getMovies(sort: Category): Flow<PagingData<MoviesItem>> {
         return Pager(
             config = PagingConfig(pageSize = PageSize.PAGE_SIZE, enablePlaceholders = false),
             pagingSourceFactory = { MoviesPagingSource(networkModule, sort) }
-        ).liveData
+        ).flow
     }
 
     override suspend fun getMovie(movieId: Int): MovieDetailsResponse =
--- app/src/main/java/ru/petrgostev/myfirstproject/network/repository/NetworkRepositoryInterface.kt
+++ app/src/main/java/ru/petrgostev/myfirstproject/network/repository/NetworkRepositoryInterface.kt
@@ -1,6 +1,5 @@
 package ru.petrgostev.myfirstproject.network.repository
 
-import androidx.lifecycle.LiveData
 import androidx.paging.PagingData
 import kotlinx.coroutines.flow.Flow
 import ru.petrgostev.myfirstproject.network.pojo.GenresItem
@@ -17,5 +16,5 @@ interface NetworkRepositoryInterface {
 
     suspend fun getImages(): ImagesResponse
 
-    fun getMovies(sort: Category): LiveData<PagingData<MoviesItem>>
+    fun getMovies(sort: Category): Flow<PagingData<MoviesItem>>
 }
